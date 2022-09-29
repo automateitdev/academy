fba467d3cd529ae7a9834aeba96b2d5674f84c12
Fazlul Hoque <sawravm299@gmail.com>
2022-09-29 09:44:59 +0600

    adminLte
    add school dashboard

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -33,6 +33,7 @@
         @include('layouts.frontend.recent_pic')
         @include('layouts.frontend.footer')
 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.2/owl.carousel.js"></script>
         <script src="{{ asset('js/app.js') }}" defer></script>
